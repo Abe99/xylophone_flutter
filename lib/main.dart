78a7e8d95ffa3e87513bf5ae21adefaebb87777c
Abe99 <i.mqami@gmail.com>
2024-05-24 04:11:45 +0100

    Create PlaySound()

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,99 @@ void main() {
 
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
-
+  void playSound(int soundNumber) {
+    final play1 = AudioPlayer();
+    play1.play(AssetSource('note$soundNumber.wav'));
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
       body: SafeArea(
-        child: Center(
-          child: TextButton(
-            onPressed: () {
-              final play1 = AudioPlayer();
-              play1.play(AssetSource('note1.wav'));
-            },
-            child: const Text('Click on Me!'),
+        child: Column(children: [
+             TextButton(
+              
+              
+          onPressed: () {
+            playSound(1);
+          },
+          child: Container(
+            width: double.infinity,
+            height: 100.0,
+            color: Colors.red,
           )
+          
+         
+          
         ),
+            TextButton(
+          onPressed: () {
+           playSound(2);
+          },
+          child: Container(
+            width: double.infinity,
+            height: 100.0,
+            color: Colors.orange,
+          ),
+          
+        ),
+            TextButton(
+          onPressed: () {
+            playSound(3);
+          },
+          child: Container(
+            width: double.infinity,
+            height: 100.0,
+            color: Colors.yellow,
+          ),
+          
+        ),
+           TextButton(
+          onPressed: () {
+            playSound(4);
+          },
+          child: Container(
+            width: double.infinity,
+            height: 100.0,
+            color: Colors.green,
+          ),
+          
+        ),
+            TextButton(
+          onPressed: () {
+            playSound(5);
+          },
+          child: Container(
+            width: double.infinity,
+            height: 100.0,
+            color: Colors.teal,
+          ),
+          
+        ),
+            TextButton(
+          onPressed: () {
+          playSound(6);
+          },
+          child: Container(
+            width: double.infinity,
+            height: 100.0,
+            color: Colors.blue,
+          ),
+          
+        ),
+            TextButton(
+          onPressed: () {
+            playSound(7);
+          },
+          child: Container(
+            width: double.infinity,
+            height: 100.0,
+            color: Colors.purple,
+          ),
+          
+        ),
+        ],)
+     
       ),
     ));
   }
